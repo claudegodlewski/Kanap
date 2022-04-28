7cce9b3ad6091cce1436b956e3d537dc5c765942
Claude Godlewski <96684710+claudegodlewski@users.noreply.github.com>
2022-04-28 16:47:54 +0200

    Ajout fonctions images valeurs couleurs articles

--- front/js/product.js
+++ front/js/product.js
@@ -1,16 +1,57 @@
-// https://www.sitepoint.com/get-url-parameters-with-javascript/
-// https://waytolearnx.com/2019/10/comment-recuperer-les-parametres-durl-en-javascript.html
+// Gestion de l'ID utilisateur
+var idUtilisateur = new URL(location.href).searchParams.get("_id")
+fetch(`http://localhost:3000/api/products/${idUtilisateur}`)
 
-// Récupération des informations du service web.
-fetch("http://localhost:3000/api/products")
-
+// Gestion de la requête et récupération des données (voir fonctions ci-dessous)
 .then(function(res) {
     if (res.ok) {
     return res.json();
     }
 })
+.then(function(data) {
+    images(data);
+    valeurs(data);
+    couleurs(data);
 
-var str = windows.location.search;
-var url = new URL(str);
-var identifiantProduit = url.searchParams.get("id");
-console.log(identifiantProduit);
+        // Gestion des images
+        function images(index) {
+            var classeDesImages = document.getElementsByClassName("item__img")[0]
+            var elementImg = document.createElement('img')
+            elementImg.alt = data.altTxt
+            elementImg.src = data.imageUrl
+            classeDesImages.appendChild(elementImg)
+        }
+
+        // Gestion des valeurs
+        function valeurs(index) {
+            var IdDesTitres = document.getElementById("title")
+            IdDesTitres.innerText = data.name
+            var IdDesDescriptions = document.getElementById("description")
+            IdDesDescriptions.innerText = data.description
+            var IdDesPrix = document.getElementById("price")
+            IdDesPrix.innerText = data.price
+        }
+
+        // Gestion des couleurs
+        function couleurs(index) {
+        let valeursDesCouleurs = data.colors
+        for(let i = 0; i < valeursDesCouleurs.length; i++) {
+            let IdDesCouleurs = document.getElementById('colors')
+            let optionDesCouleurs = document.createElement("option")
+            IdDesCouleurs.appendChild(optionDesCouleurs) 
+            optionDesCouleurs.innerText = (valeursDesCouleurs[i])
+            optionDesCouleurs.data = (valeursDesCouleurs[i])
+            const selectionCouleur = document.getElementById('colors')  
+        }
+    }
+
+        // Gestion du nombre d'articles choisis
+        function articles(index) {
+        const quantiteArticles = document.getElementById('quantity')
+        }
+
+})
+
+.catch(function(err) {
+    // Une erreur est survenue 
+})
