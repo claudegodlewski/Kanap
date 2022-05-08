57e196223d6fffd8b89586efae1c0e621e421822
Claude Godlewski <96684710+claudegodlewski@users.noreply.github.com>
2022-05-08 20:36:26 +0200

    Réécriture / changement de logique pour product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,96 +1,105 @@
 // Gestion de l'ID utilisateur
-const idUtilisateur = new URL(location.href).searchParams.get("_id");
+const idUtilisateur = new URL(location.href).searchParams.get('_id');
 
+// Récupération des informations au format JSON
 fetch(`http://localhost:3000/api/products/${idUtilisateur}`)
 
-// Gestion de la requête et récupération des données (voir fonctions ci-dessous)
-.then(function(res) {
-    if (res.ok) {
+  .then((res) => {
     return res.json();
-    }
-})
-.then(function(data) {
+  })
+  .then((maValeur) => {
 
-valeur(data)
-images(data)
-couleurs(data)
+// Ajout des informations dans le code HTML
+    document.querySelector('.item__img').innerHTML = `<img src="${maValeur.imageUrl}" alt="${maValeur.altTxt}">`;
+    document.getElementById('title').innerText = maValeur.name;
+    document.getElementById('price').innerText = maValeur.price;
+    document.getElementById('description').innerText = maValeur.description;
 
-    function valeur (data) {
-  // Gestion des valeurs
-            let IdDesTitres = document.getElementById("title");
-            IdDesTitres.innerText = data.name;
-            let IdDesDescriptions = document.getElementById("description");
-            IdDesDescriptions.innerText = data.description;
-            let IdDesPrix = document.getElementById("price");
-            IdDesPrix.innerText = data.price;
-    }
-  
-    function images (data) {
-        // Gestion des images
-            let classeDesImages = document.getElementsByClassName("item__img")[0];
-            let elementImg = document.createElement('img');
-            elementImg.alt = data.altTxt;
-            elementImg.src = data.imageUrl;
-            classeDesImages.appendChild(elementImg);
-    }
+    maValeur.colors.forEach((color) => {
+      let optionsCouleurs = document.createElement("option");
+      optionsCouleurs.innerText = `${color}`;
+      optionsCouleurs.value = `${color}`;
 
-    function couleurs (data) {
-        // Gestion des couleurs
-        let valeursDesCouleurs = data.colors;
-        for(let i = 0; i < valeursDesCouleurs.length; i++) {
-            let IdDesCouleurs = document.getElementById('colors');
-            let optionDesCouleurs = document.createElement("option");
-            IdDesCouleurs.appendChild(optionDesCouleurs);
-            optionDesCouleurs.innerText = (valeursDesCouleurs[i]);
-            optionDesCouleurs.value = (valeursDesCouleurs[i]);
-        }
-    }
-
-    const selectionCouleur = document.getElementById('colors');
-    var articlesQuantite = document.getElementById('quantity');
-    let boutonActivation = document.getElementById('addToCart');
-
-    boutonActivation.addEventListener('click', () => {
-
-        // console.log (articlesQuantite.data)
-        // console.log (articlesQuantite.value)
-
-        if (Number(articlesQuantite.value) < 1 ||
-        Number(articlesQuantite.value) > 100 ||
-        !selectionCouleur.value ||
-        !Number(articlesQuantite.value)
-        )
-        {
-            alert("Veuillez choisir une couleur, et un nombre entre 1 et 100");
-        }
+      let idColors = document.getElementById('colors');
+      idColors.appendChild(optionsCouleurs);
     
-    let articlesObjet = {};
-    articlesObjet.Identifiant = idUtilisateur;
-    articlesObjet.Couleur = selectionCouleur.data;
-    articlesObjet.Nombre = Number(articlesQuantite.data);
+    });
 
-    // Gestion du panier et création d'un tableau 
-    let panier = JSON.parse(localStorage.getItem('panier')) || []
-        console.log(panier)
-    for(let i = 0; i < panier.length; i++) {
-        let contenu = panier[i]
-       
-        if(contenu.color === articlesObjet.color && articlesObjet.Identifiant === contenu.Identifiant) {
-            panier[i].Nombre += articlesObjet.Nombre
-        }
+// Fonction: récupération du panier du localStorage
+    function panierLocalStorage() {
+      return localStorage.getItem("panier");
     }
 
-    // Gestion du panier et import de l'objet dans le tableau
-        panier.push(articlesObjet);
+ // Fonction: anti-doublons pour le panier
+ // source pour findIndex: (https://www.w3schools.com/jsref/jsref_findindex.asp)
+    function antiDoublons(article) {
+      const tableauPourObjet = JSON.parse(panierLocalStorage());
+      let verifierIndex = tableauPourObjet.findIndex((object) => object.id == article.id && object.color == article.color);
+      return verifierIndex;
+    }
 
-    // Gestion du panier, conversion, et envoi vers localStorage
-        const conversion = JSON.stringify(panier);
-        localStorage.setItem('panier', conversion);
+// Fonction: stockage du panier
+    function stockageObjet(tableauPourObjet) {
+      localStorage.setItem("panier", JSON.stringify(tableauPourObjet));
+    }
 
-    })
+// Fonction: ajout d'un article dans le panier
+    function ajoutArticle(article) {
+      const contenu = panierLocalStorage();
 
-})
+      if (!contenu) {
+        let tableauPourObjet = [];
+        tableauPourObjet.push(article);
+        stockageObjet(tableauPourObjet);
+      } else {
+        tableauPourObjet = JSON.parse(contenu);
+        const articlesIdentiques = antiDoublons(article);
 
-.catch(function(err) {
-    // Une erreur est survenue 
+        if (articlesIdentiques >= 0) {
+          tableauPourObjet[articlesIdentiques].quantity = String(
+            parseFloat(tableauPourObjet[articlesIdentiques].quantity) +
+              parseFloat(article.quantity)
+          );
+          tableauPourObjet[articlesIdentiques].totalPrice = String(
+            parseFloat(tableauPourObjet[articlesIdentiques].totalPrice) +
+              parseFloat(article.price)
+          );
+        } else {
+          tableauPourObjet.push(article);
+        }
+        stockageObjet(tableauPourObjet);
+      }
+    }
+
+// Fonction: création d'un article
+    function creerArticle() {
+      const quantity = document.getElementById('quantity');
+      const color = document.getElementById('colors');
+
+      if (color.value == "" || quantity.value < 1 || quantity.value > 100) {
+        {
+            alert("Choisissez une couleur et un nombre entre 1 et 100.");
+        }
+      } else {
+        let article = {
+          id: `${idUtilisateur}`,
+          name: `${maValeur.name}`,
+          color: `${color.value}`,
+          quantity: `${quantity.value}`,
+          totalPrice: String(`${quantity.value}` * `${maValeur.price}`),
+          price: `${maValeur.price}`,
+          image: `${maValeur.imageUrl}`,
+          altTxt: `${maValeur.altTxt}`,
+        };
+        ajoutArticle(article);
+      }
+    }
+
+//Event Listener sur le bouton
+    const bouton = document.getElementById('addToCart');
+    bouton.addEventListener("click", creerArticle);
+  })
+
+  .catch(function(err) {
+// Une erreur est survenue 
 })
