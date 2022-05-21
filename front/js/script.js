6515cea4f49261b6c85243a35ccc9daeac3ecf15
Claude Godlewski <96684710+claudegodlewski@users.noreply.github.com>
2022-05-21 02:58:27 +0200

    Modifications mineures

--- front/js/script.js
+++ front/js/script.js
@@ -1,14 +1,14 @@
 // Récupération des informations du service web.
 fetch("http://localhost:3000/api/products")
 
-// Récupération du résultat au format JSON et vérification du succès de la requête: "res.json" et "res.ok".
+// Récupération au format JSON et vérification du succès de la requête.
 .then(function(res) {
   if (res.ok) {
     return res.json();
     }
 })
 
-// Récupération dans la valeur dans "produits" et utilisation de la fonction "accueil".
+// Récupération de la valeur dans "produits" et utilisation de la fonction "accueil".
 .then(function(produits) {
   console.log(produits);
     accueil(produits);
@@ -20,7 +20,7 @@ fetch("http://localhost:3000/api/products")
 })
 
 // Fonction: affichage des produits sur la page d'accueil.
-function accueil(index) {
+function accueil(a) {
 
 // DOM: Séléction de l'élément ayant l'ID "items".
 const articleTraitement = document.getElementById("items");
@@ -28,10 +28,11 @@ const articleTraitement = document.getElementById("items");
 /* Boucle: récupération de _id, imageUrl, altTxt, name, et description pour chaque produit.
 Ajout dans la page d'accueil des éléments avec innerHTML.
 */
-for (let article of index) {
+
+for (let article of a) {
     articleTraitement.innerHTML += `<a href="./product.html?_id=${article._id}">
     <article>
-      <img src="${article.imageUrl}" alt="${article.altTxt}">
+      <img src= "${article.imageUrl}" alt="${article.altTxt}">
       <h3 class="productName">${article.name}</h3>
       <p class="productDescription">${article.description}</p>
     </article>
