82651d4f71fcc35a9e2eb9baad11539e0bacd2a5
Claude Godlewski <96684710+claudegodlewski@users.noreply.github.com>
2022-05-21 03:08:19 +0200

    Page de confirmation et nettoyage localStorage

---
+++ front/js/confirmation.js
@@ -0,0 +1,11 @@
+// Récupération de l'identifiant de la commande
+const idCommande = new URL(location.href).searchParams.get('id');
+
+console.log(idCommande)
+
+// Affichage du numéro de commande
+let selectionOrderId = document.getElementById("orderId");
+selectionOrderId.innerText = idCommande;
+
+// Suppression du panier dans le localStorage
+localStorage.clear();
