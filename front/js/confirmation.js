6f573a3148fd3e9ecf379383201454379df86da7
Claude Godlewski <96684710+claudegodlewski@users.noreply.github.com>
2022-06-01 18:57:01 +0200

    Ajout de commentaires, indentation.

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,11 +1,11 @@
-// Récupération de l'identifiant de la commande
+// Récupération de l'identifiant de la commande.
 const idCommande = new URL(location.href).searchParams.get('id');
 
 console.log(idCommande)
 
-// Affichage du numéro de commande
+// Affichage du numéro de commande.
 let selectionOrderId = document.getElementById("orderId");
 selectionOrderId.innerText = idCommande;
 
-// Suppression du panier dans le localStorage
+// Suppression du panier dans le localStorage.
 localStorage.clear();
