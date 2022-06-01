b1997cf47ec0e2fbfc9f15ea3a129a4a13613103
Claude Godlewski <96684710+claudegodlewski@users.noreply.github.com>
2022-06-01 18:55:38 +0200

    Retrait du prix, ajout de commentaires.

--- front/js/product.js
+++ front/js/product.js
@@ -5,17 +5,17 @@ const idUtilisateur = new URL(location.href).searchParams.get('_id');
 fetch(`http://localhost:3000/api/products/${idUtilisateur}`)
 
 // Première promesse: récupération des détails de l'article choisi par le client.
-  .then((res) => {
-    return res.json();
-  })
+.then((res) => {
+  return res.json();
+})
 
 // Seconde promesse: récupération de la valeur dans "produit" et ajout des informations dans le code HTML.
-  .then((produit) => {
+.then((produit) => {
 
-    document.querySelector('.item__img').innerHTML = `<img src="${produit.imageUrl}" alt="${produit.altTxt}">`;
-    document.getElementById('title').innerText = produit.name;
-    document.getElementById('price').innerText = produit.price;
-    document.getElementById('description').innerText = produit.description;
+  document.querySelector('.item__img').innerHTML = `<img src="${produit.imageUrl}" alt="${produit.altTxt}">`;
+  document.getElementById('title').innerText = produit.name;
+  document.getElementById('price').innerText = produit.price;
+  document.getElementById('description').innerText = produit.description;
 
     produit.colors.forEach((a) => {
 
@@ -28,97 +28,97 @@ fetch(`http://localhost:3000/api/products/${idUtilisateur}`)
     
     });
 
-// Fonction: stockage du panier.
-function stockagePanierLocalStorage(a) {
-  localStorage.setItem("panierKanap", JSON.stringify(a));
-}
+  // Fonction: stockage du panier.
+  function stockagePanierLocalStorage(a) {
+    localStorage.setItem("panierKanap", JSON.stringify(a));
+  }
 
-// Fonction: récupération du panier depuis localStorage.
-function recuperationLocalStorage() {
-  return localStorage.getItem("panierKanap");
-}
+  // Fonction: récupération du panier depuis localStorage.
+  function recuperationLocalStorage() {
+    return localStorage.getItem("panierKanap");
+  }
 
-// Fonction: anti-doublons pour le panier.
-function antiDoublons(a) { 
-  const objetATester = JSON.parse(recuperationLocalStorage());
-    let verifierIndex = objetATester.findIndex((b) => b.color == a.color && b.id == a.id);
-    //console.log(verifierIndex); // Sans doublons donne -1 et avec doublons donne 0.
-    return verifierIndex;
-}
+  // Fonction: anti-doublons pour le panier.
+  function antiDoublons(a) { 
+    const objetATester = JSON.parse(recuperationLocalStorage());
+      let verifierIndex = objetATester.findIndex((b) => b.color == a.color && b.id == a.id);
+      //console.log(verifierIndex); // Sans doublons donne -1 et avec doublons donne 0.
+      return verifierIndex;
+  }
 
-// Fonction: création d'un article.
-function creerArticle() {
+  // Fonction: création d'un article.
+  function creerArticle() {
 
-  const quantity = document.getElementById('quantity');
-  const color = document.getElementById('colors');
+    const quantity = document.getElementById('quantity');
+    const color = document.getElementById('colors');
 
-  if (color.value == "" || quantity.value < 1 || quantity.value > 100 || quantity.value.includes(".")) {
+    if (color.value == "" || quantity.value < 1 || quantity.value > 100 || quantity.value.includes(".")) {
 
-    {
-        alert("Choisissez une couleur et un nombre entier entre 1 et 100.");
+      {
+          alert("Choisissez une couleur et un nombre entier entre 1 et 100.");
+      }
+
+    } else {
+
+    // Création d'un objet sans stockage de prix (sécurité)
+    let article = {
+      id: `${idUtilisateur}`,
+      name: `${produit.name}`,
+      color: `${color.value}`,
+      quantity: `${quantity.value}`,
+      //totalPrice: String(`${quantity.value}` * `${produit.price}`),
+      //price: `${produit.price}`,
+      image: `${produit.imageUrl}`,
+      altTxt: `${produit.altTxt}`,
+    };
+
+    // Récupération de ce qui est contenu dans le localStorage.
+    const contenu = recuperationLocalStorage();
+
+    if (!contenu) {
+
+      // Création d'un tableau.
+      let tableauPourObjet = [];
+
+      // Ajout de l'objet article dans le tableau.
+      tableauPourObjet.push(article);
+
+      // Ajout du tableau contenant l'objet dans le localStorage.
+      stockagePanierLocalStorage(tableauPourObjet);
+
+    } else {
+
+      // Conversion de l'objet JSON au format JavaScript.
+      tableauPourObjet = JSON.parse(contenu);
+
+      const articlesIdentiques = antiDoublons(article);
+      //console.log(articlesIdentiques);
+
+      if (articlesIdentiques >= 0) { // Rappel pour articlesIdentiques (sans doublons donne -1 et avec doublons donne 0).
+
+        tableauPourObjet[articlesIdentiques].quantity = parseFloat(tableauPourObjet[articlesIdentiques].quantity) + parseFloat(article.quantity); // Ajout des quantités objets localStorage + objets JS 
+
+        //console.log(tableauPourObjet[articlesIdentiques].quantity);
+
+        //tableauPourObjet[articlesIdentiques].totalPrice = parseFloat(tableauPourObjet[articlesIdentiques].totalPrice) + parseFloat(article.price);
+
+      } else {
+
+        tableauPourObjet.push(article);
+
+      }
+
+        stockagePanierLocalStorage(tableauPourObjet);
+
+      {
+          alert("Produit(s) ajouté(s).");
+      }
+
+    } 
+        
     }
 
-  } else {
-
-// Création d'un objet sans stockage du prix (sécurité)
-let article = {
-  id: `${idUtilisateur}`,
-  name: `${produit.name}`,
-  color: `${color.value}`,
-  quantity: `${quantity.value}`,
-  //totalPrice: String(`${quantity.value}` * `${produit.price}`),
-  //price: `${produit.price}`,
-  image: `${produit.imageUrl}`,
-  altTxt: `${produit.altTxt}`,
-};
-
-// Récupération de ce qui est contenu dans le localStorage.
-const contenu = recuperationLocalStorage();
-
-if (!contenu) {
-
-  // Création d'un tableau.
-  let tableauPourObjet = [];
-
-  // Ajout de l'objet article dans le tableau.
-  tableauPourObjet.push(article);
-
-  // Ajout du tableau contenant l'objet dans le localStorage.
-  stockagePanierLocalStorage(tableauPourObjet);
-
-} else {
-
-  // Conversion de l'objet JSON au format JavaScript.
-  tableauPourObjet = JSON.parse(contenu);
-
-  const articlesIdentiques = antiDoublons(article);
-  //console.log(articlesIdentiques);
-
-  if (articlesIdentiques >= 0) { // Rappel pour articlesIdentiques (sans doublons donne -1 et avec doublons donne 0).
-
-    tableauPourObjet[articlesIdentiques].quantity = parseFloat(tableauPourObjet[articlesIdentiques].quantity) + parseFloat(article.quantity); // Ajout des quantités objets localStorage + objets JS 
-
-    //console.log(tableauPourObjet[articlesIdentiques].quantity);
-
-    //tableauPourObjet[articlesIdentiques].totalPrice = parseFloat(tableauPourObjet[articlesIdentiques].totalPrice) + parseFloat(article.price);
-
-  } else {
-
-    tableauPourObjet.push(article);
-
-  }
-
-    stockagePanierLocalStorage(tableauPourObjet);
-
-  {
-      alert("Produit(s) ajouté(s).");
-  }
-
-} 
-    
-}
-
-} // Fin de creerArticle()
+  } // Fin de creerArticle().
 
 
 
